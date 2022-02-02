f64b7a8efd3a47f860f195524689062ca291da15
vantheav <vantheav.thav@student.passerellesnumeriques.org>
2022-02-02 15:56:24 -0800

    Vuetiry

---
+++ book/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- App.vue -->
+
+    <v-app>
+      <v-navigation-drawer app v-model="isNav">
+        <nav></nav>
+      </v-navigation-drawer>
+
+      <v-app-bar app>
+        <!-- -->
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-main>
+
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+
+          <!-- If using vue-router -->
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </v-app>
+</template>
+
+<script>
+import navigation from './components/nav.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    'nav': navigation,
+  },
+
+  data: () => ({
+    isNav: false,
+  }),
+};
+</script>
